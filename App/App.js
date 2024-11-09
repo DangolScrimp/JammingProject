3904be7adfa66677634a108ead3cc0d6720ce0f8
Atticus Boyd <atticus.boyd@gmail.com>
2024-11-09 15:22:13 -0600

    create App component allowing for playlist creation and save to spotify render SearchBar SearchResults and Playlist components

--- App/App.js
+++ App/App.js
@@ -1,16 +1,68 @@
-import React from 'react';
-import searchBar from '../searchBar/searchBar.js';
+import React, {useState, useCallback} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      
-    </div>
-  );
-}
+import Spotify from '../App/Utility/Spotify.js';
+import SearchResults from '../App/searchResults/searchResults.js';
+import Playlist from '../App/playlist/playlist.js';
+import SearchBar from '../searchBar/searchBar.js';
+
+
+const App = () => {
+
+    const [searchResults, setSearchResults] = useState([]);
+    const [playlistName, setPlaylistName] = useState('New Playlist');
+    const [playlistTracks, setPlaylistTracks] = useState([]);
+
+    const search = useCallback((text) => {
+      Spotify.search(text).then(setSearchResults);
+    }, []);
+
+    const addTrack = useCallback((track) => {
+      if (playlistTracks.some((savedTrack) => savedTrack.id === track.id))
+        return;
+      setPlaylistTracks((prevTracks) => [...prevTracks, track]);
+    }, [playlistTracks]);
+
+    const removeTrack = useCallback((track) => {
+      setPlaylistTracks((prevTracks) => 
+        prevTracks.filter((currentTrack) => currentTrack.id !== track.id)
+      );
+    }, []);
+
+    const updatePlaylistName = useCallback((name) => {
+      setPlaylistName(name);
+    }, []);
+
+    const savePlaylist = useCallback(() => {
+      const trackUris = playlistTracks.map((track) => track.uri);
+      Spotify.savePlaylist(playlistName, trackUris).then(() => {
+        setPlaylistName('New Playlist');
+        setPlaylistTracks([]);
+      });
+    }, [playlistName, playlistTracks]);
+
+
+
+    return (
+      <div className="App">
+        <h1>
+          Ja<span className='highlight'>mmm</span>ing
+        </h1>
+        <div className='App' >
+          <SearchBar onSearch={search} />
+          <div className='App-playlist'>
+            <SearchResults searchResults={searchResults} onAdd={addTrack}/>
+            <Playlist 
+              playlistName={playlistName}
+              playlistTracks={playlistTracks}
+              onNameChange={updatePlaylistName}
+              onRemove={removeTrack}
+              onSave={savePlaylist}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 export default App;

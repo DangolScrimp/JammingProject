8bcb5f909fdb3d70fb0ebe1d11a33ef4051e2317
Atticus Boyd <atticus.boyd@gmail.com>
2024-11-09 15:18:17 -0600

    create Tracklist component and return Track Component

--- App/tracklist/tracklist.js
+++ App/tracklist/tracklist.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import Track from '../track/track';
+import './trackList.css'
 
-function tracklist() {
-    
-}
+const TrackList = (props) => {
+    return (
+        <div className='TrackList'>
+            {props.tracks.map((track) => {
+                return (
+                    <Track 
+                      track={track}
+                      key={track.id}
+                      onAdd={props.onAdd}
+                      isRemoval={props.isRemoval}
+                      onRemove={props.onRemove}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default TrackList;

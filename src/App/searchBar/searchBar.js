3f056c20a3b61c86443d20f5c94f65e5bf29f7dc
Atticus Boyd <atticus.boyd@gmail.com>
2024-11-21 19:27:01 -0600

    save search in local storage and try to fix refresh bug to no avail

--- src/App/searchBar/searchBar.js
+++ src/App/searchBar/searchBar.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import '../searchBar/searchBar.css';
 
 
@@ -6,21 +6,29 @@ function SearchBar(props) {
   const { onSearch } = props;
   const [text, setText] = useState('');
 
+  useEffect(() => {
+    const savedText = localStorage.getItem('searchText');
+    if (savedText) {
+      setText(savedText);
+    }
+  }, []);
+
   const handleTextChange = useCallback((event) => {
     setText(event.target.value);
   }, []);
 
   const search = useCallback((event) => {
     event.preventDefault();
+    localStorage.setItem('searchText', text);
     onSearch(text);
-  }, [onSearch, text]);
+  }, [onSearch, text]); 
 
 
   return (
     <div className="SearchBar">
       <form onSubmit={search} classname="form">
         <input placeholder="Enter A Song Title" onChange={handleTextChange} type='text'/>
-        <button className="SearchButton" type='submit'>
+        <button className="SearchButton" type='submit' onclick={search}>
           SEARCH
         </button>
       </form>
